ea65ea379ca33d8eabbf92f00b59b339ca83b979
Hector Zarco <hezarco@gmail.com>
2017-01-08 16:13:45 +0100

    Force exported functions

--- palindrome/loader.js
+++ palindrome/loader.js
@@ -1,35 +1,22 @@
-function loadModule(filename, imports) {
+function loadModule(filename) {
   return fetch(filename)
     .then(response => response.arrayBuffer())
     .then(buffer => WebAssembly.compile(buffer))
     .then(module => {
-      imports = imports || {};
-      imports.env = imports.env || {};
-      imports.env.memoryBase = imports.env.memoryBase || 0; //0 | 1024
-      imports.env.tableBase = imports.env.tableBase || 0;
-      imports.env._strlen = function(a, b, c) {
-        console.log(a, b, c);
-        // debugger
-        // return 0;
+      const imports = {
+        env: {
+          memoryBase: 0, //0 | 1024
+          tableBase: 0,
+          memory: new WebAssembly.Memory({
+            initial: 256
+          }),
+          table: new WebAssembly.Table({
+            initial: 0,
+            element: 'anyfunc'
+          })
+        }
       };
-      imports.env._printf = function(a, b, c) {
-        console.log(a, b, c);
-        // debugger;
-      };
-
-      if (!imports.env.memory) {
-        imports.env.memory = new WebAssembly.Memory({
-          initial: 256
-        });
-      }
-
-      if (!imports.env.table) {
-        imports.env.table = new WebAssembly.Table({
-          initial: 0,
-          element: 'anyfunc'
-        });
-      }
-
+      
       return new WebAssembly.Instance(module, imports);
     });
 }
